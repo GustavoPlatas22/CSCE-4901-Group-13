bc0e5cba2d3bd21d3fd5eb3f1fca6f89861c4ca1
OzielPlatas <ozielplatas@gmail.com>
2024-10-29 10:36:11 -0500

    added firebase config to environment.ts

---
+++ research-finder/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAn3B1UoeUJG3r5BX7vicSDb-PA9AOEzHo",
+        authDomain: "research-finder-33c2b.firebaseapp.com",
+        projectId: "research-finder-33c2b",
+        storageBucket: "research-finder-33c2b.appspot.com",
+        messagingSenderId: "223873501213",
+        appId: "1:223873501213:web:549f64292f3e6c95aa9ef4",
+        measurementId: "G-7WTRB4LCNJ"
+    }
+};
